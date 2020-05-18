c10405ca66de68c301de493e66f26a0ff2ec9f5a
Frank <frankchen1328@gmail.com>
2020-05-18 23:17:09 +0800

    init update

--- src/part1_service-register-discovery/movie-service/src/main/java/com/mbps/cd/movieservice/MovieServiceApplication.java
+++ src/part1_service-register-discovery/movie-service/src/main/java/com/mbps/cd/movieservice/MovieServiceApplication.java
@@ -8,9 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * @author EdisonZhou
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MovieServiceApplication {
